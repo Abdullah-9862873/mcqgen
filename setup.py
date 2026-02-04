d67199a2a6d1f84019e4df4dddd98036384ad119
Abdullah-9862873 <ag9862873@gmail.com>
2026-02-04 22:36:43 +0500

    Dependencies-installed

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
 # To install local package in my virtual environment I'd be using this setup.py file
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mcqgen",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "openRouter",
+        "streamlit",
+        "langchain",
+        "pyPDF2",
+        "python-dotenv",
+    ],
+
+)
